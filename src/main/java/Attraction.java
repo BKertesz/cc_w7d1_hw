8e4d5d57658c418d22737872919e6a0630963cb8
BKertesz <kertesz.balazs92@gmail.com>
2018-04-16 16:37:40 +0100

    Added classes for most object

---
+++ src/main/java/Attraction.java
@@ -0,0 +1,9 @@
+public abstract class Attraction {
+
+    private String name;
+
+    public Attraction(String name) {
+        this.name = name;
+    }
+
+}
--- src/test/java/VisitorTest.java
+++ src/test/java/VisitorTest.java
@@ -9,5 +9,5 @@ public class VisitorTest {
         visitor1 = new Visitor(21,178,200);
     }
 
-    
+
 }
