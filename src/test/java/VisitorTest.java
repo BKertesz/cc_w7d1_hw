a7e49b41910fa38dc0a5b8fa3bcab986bc9b4111
BKertesz <kertesz.balazs92@gmail.com>
2018-04-16 16:30:10 +0100

    First commit

---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class VisitorTest {
+
+    Visitor visitor1;
+
+    @Before
+    public void setUp() throws Exception {
+        visitor1 = new Visitor(21,178,200);
+    }
+
+    
+}
